986774759f1b85a12e5f68fd7f7bb6279f789b74
jhapriyansh <69211610+jhapriyansh@users.noreply.github.com>
2024-07-15 17:26:41 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 public class Main
 {
+// Java is case-sensitive
 public static void main(String[] args)
 {
 System.out.println("Hello World");
+// This code simply prints Hello World.
 }
+/* This showcases an example of a 
+MULTI-LINE COMMENT */
 }
