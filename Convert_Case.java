b1b08b2cf0e1cc3aef30d57be86bc26fc9dc3bec
jhapriyansh <69211610+jhapriyansh@users.noreply.github.com>
2024-07-17 15:34:38 +0530

    Add files via upload

---
+++ Convert_Case.java
@@ -0,0 +1,7 @@
+public class Convert_Case{
+public static void main(String[] args){
+String txt = "lower case to upper case and back to lower case.";
+System.out.println(txt.toUpperCase());
+System.out.println(txt.toLowerCase());
+}
+}
