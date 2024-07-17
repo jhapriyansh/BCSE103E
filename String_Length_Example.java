c5d3a23a55ecc19493a50160805a04786fb6edc2
jhapriyansh <69211610+jhapriyansh@users.noreply.github.com>
2024-07-17 15:30:58 +0530

    Add files via upload

---
+++ String_Length_Example.java
@@ -0,0 +1,6 @@
+public class String_Length_Example{
+public static void main(String[] args){
+String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+System.out.println("The length of the string is: "+txt.length());
+}
+}
