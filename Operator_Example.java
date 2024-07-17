98397055f12243ba65cefb30653ae10ed0aa100e
jhapriyansh <69211610+jhapriyansh@users.noreply.github.com>
2024-07-17 15:11:03 +0530

    Add files via upload

---
+++ Operator_Example.java
@@ -0,0 +1,11 @@
+public class Operator_Example{
+public static void main(String[] args){
+int num1 = 10;
+int num2 = 20;
+String s1 = "abc";
+String s2 = "def";
+System.out.println(num1+num2);
+System.out.println(num1+s1);
+System.out.println(s1+s2);
+}
+}
