4ad7c4e1046fd9434c8ffdd372bf5e877e5bb70a
jhapriyansh <69211610+jhapriyansh@users.noreply.github.com>
2024-07-17 14:55:07 +0530

    Add files via upload

---
+++ Type_Conversion.java
@@ -0,0 +1,9 @@
+public class Type_Conversion{
+	public static void main(String[] args){
+		int num = 9;
+		float number = num;
+		// Widening type cast.
+		System.out.println(num);
+		System.out.println(number);
+	}
+}
