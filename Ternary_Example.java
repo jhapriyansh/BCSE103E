18962f7f3d7d32f68394e41e5a4d01c6181982a6
jhapriyansh <69211610+jhapriyansh@users.noreply.github.com>
2024-07-17 15:26:07 +0530

    Add files via upload

---
+++ Ternary_Example.java
@@ -0,0 +1,10 @@
+public class Ternary_Example{
+public static void main(String[] args){
+int x = 20;
+int y = (x==1)?61:90;
+System.out.println("The value of y is: "+y);
+y = (x==20)?61:90;
+System.out.println("The value of y is: "+y);
+}
+}
+
