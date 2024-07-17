be60dd7f069d876f5f151c986991420a5d8a3054
jhapriyansh <69211610+jhapriyansh@users.noreply.github.com>
2024-07-17 14:52:56 +0530

    Add files via upload

--- Vari_Example.java
+++ Vari_Example.java
@@ -3,6 +3,12 @@ public class Vari_Example
 public static void main(String[] args)
 {
 String name = "John";
-System.out.println(name);
+// final int num = 34;
+// trying to change a final variable leads to error.
+int num = 34;
+System.out.println("Name: "+name);
+System.out.println("Number: "+num);
+num = 45;
+System.out.println("Updated Number: "+num);
 }
 }
