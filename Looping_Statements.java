e9a57d50ea9bc8726ebf911d87b94e89b6fc7ac7
jhapriyansh <69211610+jhapriyansh@users.noreply.github.com>
2024-07-22 17:19:36 +0530

    Add files via upload

---
+++ Looping_Statements.java
@@ -0,0 +1,33 @@
+// Upload
+public class Looping_Statements{
+    public static void main(String[] args){
+        // int i = 0;
+        // while(i<5){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+
+        // do{
+        //     System.out.println(i);
+        //     i++;
+        // }while(i<8);
+
+    
+        //     int i = 0;
+        // for(;;){
+        //     if(i>4){
+        //         break;
+        //     }
+        //     System.out.println(i);
+        //     i++;
+        // }
+        
+        
+        // String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        // for(String i:cars){
+        //     System.out.println(i);
+        // }
+    }
+}
+// javac Looping_Statements.java && java Looping_Statements
