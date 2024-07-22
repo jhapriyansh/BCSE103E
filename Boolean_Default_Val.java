96dff3712ca3a0c785f201f578a2354bcfb3704f
jhapriyansh <69211610+jhapriyansh@users.noreply.github.com>
2024-07-22 16:49:58 +0530

    Add files via upload

---
+++ Boolean_Default_Val.java
@@ -0,0 +1,12 @@
+// Upload
+public class Boolean_Default_Val{
+public static void main(String[] args){
+Boolean def = true;
+// No default value.
+System.out.println(def);
+int x = 10;
+int y = 9;
+System.out.println(x>y);
+System.out.println(10==15);
+}
+}
