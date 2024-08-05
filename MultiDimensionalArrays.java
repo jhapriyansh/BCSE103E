ae455264a29c79af894fd49d40af331a3a96324d
jhapriyansh <69211610+jhapriyansh@users.noreply.github.com>
2024-08-05 16:53:33 +0530

    Add files via upload

---
+++ MultiDimensionalArrays.java
@@ -0,0 +1,14 @@
+// 5 Aug
+public class MultiDimensionalArrays {
+    public static void main(String[] args) {
+        // 2d
+        // int[][] arr = new int[10][20];
+        // arr[0][0] = 1;
+        // System.out.println("Element at arr[0][0]: " + arr[0][0]);
+
+        // 3d
+        int[][][] arr = new int[10][20][30];
+        arr[0][0][0] = 1;
+        System.out.println("Element at arr[0][0][0]: " + arr[0][0][0]);
+    }
+}
