b521b032d4cd60b4c6b3648fc9b57769c2d16ce5
jhapriyansh <69211610+jhapriyansh@users.noreply.github.com>
2024-08-07 15:33:06 +0530

    Add files via upload

---
+++ Constructors.java
@@ -0,0 +1,13 @@
+// 7 Aug
+public class Constructors {
+    int x;
+
+    public Constructors() {
+        x = 5;
+    }
+
+    public static void main(String[] args) {
+        Constructors o1 = new Constructors();
+        System.out.println(o1.x);
+    }
+}
