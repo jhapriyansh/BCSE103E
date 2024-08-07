e84861aa1fe7b18828c3a0f6c14444c7cd7afcb5
jhapriyansh <69211610+jhapriyansh@users.noreply.github.com>
2024-08-07 15:18:03 +0530

    Add files via upload

---
+++ MethodsJava.java
@@ -0,0 +1,9 @@
+// 7 Aug
+public class MethodsJava {
+    static void myMethod() {
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        myMethod();
+    }
+}
