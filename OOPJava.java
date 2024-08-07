f772cede9a3a1780a39c6ecd87b2fcc8733cf3b7
jhapriyansh <69211610+jhapriyansh@users.noreply.github.com>
2024-08-07 14:56:17 +0530

    Add files via upload

---
+++ OOPJava.java
@@ -0,0 +1,11 @@
+// 7 Aug
+public class OOPJava {
+    int x = 5;
+
+    public static void main(String[] args) {
+        OOPJava obj1 = new OOPJava();
+        OOPJava obj2 = new OOPJava();
+        System.out.println(obj1.x);
+        System.out.println(obj2.x);
+    }
+}
